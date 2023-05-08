d68d8b1d5c8d4bb2e006d038f2e2036ee6d9edd9
GovardVolovets <govard.volovets@gmail.com>
2023-05-08 08:59:43 +0300

    create postmanEchoTest

---
+++ src/test/java/ru/netology/postmanEcho/postmanEchoTest.java
@@ -0,0 +1,25 @@
+package ru.netology.postmanEcho;
+
+import org.junit.jupiter.api.Test;
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.core.IsEqual.equalTo;
+
+public class postmanEchoTest {
+    @Test
+    void shouldReturnSameData() {
+        // Given - When - Then
+        // Предусловия
+        given()
+                .baseUri("https://postman-echo.com")
+                .body("10.01.2000") // отправляемые данные (заголовки и query можно выставлять аналогично)
+                // Выполняемые действия
+                .when()
+                .post("/post")
+                // Проверки
+                .then()
+                .statusCode(200)
+                .body("data", equalTo("10.01.2000"))
+        ;
+
+    }
+}
